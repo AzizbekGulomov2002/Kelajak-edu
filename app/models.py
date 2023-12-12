a049741631c0b57c0309d76aec873924eb271a71
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2023-12-12 11:12:16 +0500

    deploy

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Markaz(models.Model):
     rasm = models.ImageField(upload_to='jamoa_rasm')
     markaz_nomi = models.CharField(max_length=200)
