477b11835809c7d8e1e1dbb218492b96e1696a3a
Your Name <firdavsef700@gmail.com>
2023-12-21 23:03:22 +0500

    deploy-requirements

--- project/settings.py
+++ project/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': '95.130.227.185',
+            'USER': 'postgres',
+            'PASSWORD': 'kelajak2023',
+            'HOST': '95.130.227.185',
+            'PORT': '5432',
+        }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
