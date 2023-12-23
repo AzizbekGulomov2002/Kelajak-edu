55d7671a55e487c3d5f094da457694fc4a15445d
Smarttizim2023 <smarttizim2023@gmail.com>
2023-12-23 22:12:07 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -72,9 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
