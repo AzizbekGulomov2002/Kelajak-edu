64bbc1f955c0478ecd0f82b377f9e60fd7e3332d
Smarttizim2023 <smarttizim2023@gmail.com>
2023-12-23 22:47:14 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -72,25 +72,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kelajak_db',  # Replace 'kelajak_db' with the actual name of your PostgreSQL database
-        'USER': 'postgres',
-        'PASSWORD': 'kelajak2023',
-        'HOST': '95.130.227.185',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'kelajak_db',  # Replace 'kelajak_db' with the actual name of your PostgreSQL database
+#         'USER': 'postgres',
+#         'PASSWORD': 'kelajak2023',
+#         'HOST': '95.130.227.185',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
